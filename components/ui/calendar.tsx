59bdf7c3c53a0c93d6e6ac455af5a499d6dc1f67
Yogesh Patil <yogupatil135@gmail.com>
2024-12-30 00:29:45 +0530

    added dependcies

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -2,12 +2,16 @@
 
 import * as React from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
-import { DayPicker } from 'react-day-picker';
+import { DayPicker, CustomComponents } from 'react-day-picker';
 
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+type ExtendedComponents = CustomComponents & {
+  IconLeft?: React.ComponentType<any>;
+  IconRight?: React.ComponentType<any>;
+};
 
 function Calendar({
   className,
@@ -54,9 +58,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-      }}
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" {...props} />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" {...props} />,
+      } as ExtendedComponents} // Cast to ExtendedComponents
       {...props}
     />
   );
